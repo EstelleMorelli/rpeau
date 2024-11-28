142ddb93ae6d8a698f8e658fd48f45ec329c9e29
Estelle Morelli <estelle.morelli@oclock.school>
2024-11-28 16:03:48 +0100

    section service OK

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,14 @@
 import Footer from '../Footer/Footer';
 import './Home.scss';
 import Intro from './Intro/Intro';
+import Services from './Services/Services';
 
 function Home() {
 
   return (
     <div className="Home">
       <Intro />
+      <Services />
       <Footer />
     </div>
   );
--- src/components/Home/Services/Services.tsx
+++ src/components/Home/Services/Services.tsx
@@ -1,10 +1,41 @@
 import './Services.scss';
-
+import service1 from '../../../../public/images/service1.png'; 
+import service2 from '../../../../public/images/service2.png'; 
+import service3 from '../../../../public/images/service3.png'; 
+import service4 from '../../../../public/images/service4.png'; 
 function Services() {
 
   return (
     <div className="Services">
-        <h1>Artisan pompes de relevage </h1>
+        <h2 className='Services--maintitle'> LES SERVICES </h2>
+        <section className='Services--section'>
+          <div className='Service--logo__circle'>
+          <img className='Service--logo' src={service1} />
+          </div>
+          <h3 className='Services--subtitle'>Relevage des eaux usées</h3>
+          <p className='Services--text'>Dans le cas de sous-sols enterrés ou de bâtiments situés en contrebas par rapport au réseau d'égouts, l'installation d'une pompe électrique de relevage est indispensable.</p>
+        </section>
+        <section className='Services--section'>
+        <div className='Service--logo__circle'>
+          <img className='Service--logo' src={service2} />
+          </div>
+          <h3 className='Services--subtitle'>Récupération des eaux de pluie</h3>
+          <p className='Services--text'>La récupération de l’eau de pluie représente une solution écologique pour divers usages comme l’arrosage des jardins, le remplissage des piscines. </p>
+        </section>
+        <section className='Services--section'>
+        <div className='Service--logo__circle'>
+        <img className='Service--logo' src={service3} />
+        </div>
+          <h3 className='Services--subtitle'>Traitement et pompage de l'eau potable</h3>
+          <p className='Services--text'>Le traitement est essentiel pour garantir une distribution d’eau potable de qualité. Les surpresseurs assurent un rôle crucial en maintenant un approvisionnement optimal et une pression adéquate, particulièrement dans les bâtiments élevés ou les zones où le réseau manque de puissance.</p>
+        </section>
+        <section className='Services--section'>
+        <div className='Service--logo__circle'>
+        <img className='Service--logo' src={service4} />
+        </div>
+        <h3 className='Services--subtitle'>Entretien des installations</h3>
+        <p className='Services--text'>Les systèmes de relevage et les micro-stations requièrent un entretien annuel. Je propose des interventions pour ces équipements.</p>
+        </section>
     </div>
   );
 }
